1879414bcb448d695a01726a5f124eff0ce068cb
Gabriel Arnecke <2005294@aluno.univesp.br>
2022-05-22 22:21:04 -0300

    Atualização de paginas

--- Js/show_content.js
+++ Js/show_content.js
@@ -2,7 +2,7 @@
 
 function show_nav(){
     var box_nav = document.getElementById("navbarNavDropdown");
-    if(box_nav.classList[2] == "show"){
+    if(box_nav.classList[2] == "show" || box_nav.classList[4] == "show"){ // 2 referente ou index.html e 4 referente ao dashboard.html
         box_nav.classList.remove("show");
     }
     else{
