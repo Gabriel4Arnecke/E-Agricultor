c006290787fce19a05c83a6a92835be654c3c0c2
Gabriel Arnecke <2005294@aluno.univesp.br>
2022-05-26 23:56:51 -0300

    Added files suport for make weather API and dashboard page update

--- Suport_files/weather-app-tutorial-main/script.js
+++ Suport_files/weather-app-tutorial-main/script.js
@@ -31,7 +31,7 @@ let weather = {
     document.querySelector(".wind").innerText =
       "Wind speed: " + speed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
+    document.getElementById('main').style.backgroundImage =
       "url('https://source.unsplash.com/1600x900/?" + name + "')";
   },
   search: function () {
